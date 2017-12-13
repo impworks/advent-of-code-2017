e774445afb153bc25dd26ce9c97b1819620b78a8
Andrei Kurosh <mail@spark.ms>
2017-12-13 14:24:38 +0300

    Solution for Puzzle 13.

---
+++ 13.fs
@@ -0,0 +1,108 @@
+open System
+open System.IO
+
+type Scanner =
+    {
+        Depth: int;
+        Direction: int;
+        Position: int;
+    }
+
+let initialState =
+    let source =
+        let parse (line: string) =
+            let parts = line.Split([|": "|], StringSplitOptions.None)
+            (int parts.[0], int parts.[1])
+
+        File.ReadAllLines "13.txt"
+        |> Array.map parse
+
+    let find id =
+        source
+        |> Seq.tryFind (fun x -> fst x = id)
+        |> Option.map (fun def -> { Depth = snd def; Direction = 1; Position = 0 })
+
+    let layersCount =
+        source
+        |> Seq.map fst
+        |> Seq.max
+
+    { 0 .. layersCount }
+    |> Seq.map find
+    |> Seq.toArray
+
+let layersCount = initialState.Length
+
+let visualize (state: Scanner option []) (pos: int) (severity: int) =
+    Console.WriteLine("State {0}: (S = {1})", pos, severity)
+    for i = 0 to state.Length - 1 do
+        Console.Write("{0}: ", i)
+        match state.[i] with
+        | None -> Console.WriteLine (if pos = i then "(.)" else "...")
+        | Some sc ->
+            for j = 0 to sc.Depth - 1 do
+                let template = if pos = i && j = 0 then "({0}) " else "[{0}] "
+                let mark = if j = sc.Position then "X" else " "
+                Console.Write(template, mark)
+            Console.WriteLine ()
+    Console.WriteLine()
+
+let answer1 =
+    let step state =
+        let stepScanner scopt =
+            match scopt with
+            | None -> None
+            | Some s ->
+                let changeDir = (s.Position = 0 && s.Direction = -1) || (s.Position = s.Depth - 1 && s.Direction = 1)
+                let dir = s.Direction * (if changeDir then -1 else 1)
+                let pos = s.Position + dir
+                Some { s with Direction = dir; Position = pos }
+
+        Array.map stepScanner state
+
+    let getSeverity (state: Scanner option []) pos =
+        match state.[pos] with
+        | Some p when p.Position = 0 -> pos * p.Depth
+        | _ -> 0
+
+    let rec traverse state severity pos =
+        if pos = layersCount + 1 then
+            severity
+        else
+            let newSeverity = severity + (getSeverity state pos)
+            let newState = step state
+            let newPos = pos + 1
+            traverse newState newSeverity newPos
+
+    traverse initialState 0 0
+
+let answer2 =
+    let posAt depth time =
+        let leg = depth - 1
+        let ntime = time - 1
+        if ntime = -1 then 0
+        else
+            let forward = (ntime / leg) % 2 = 0
+            let legOffset = (ntime % leg)
+            if forward then
+                legOffset + 1
+            else
+                depth - legOffset - 2
+
+    let isWalkSafe (state:Scanner option []) delay = 
+        let isNoticed (state:Scanner option []) delay pos = 
+            match state.[pos] with
+            | None -> false
+            | Some sc ->
+                posAt (sc.Depth) (pos + delay) = 0
+
+        { 0 .. layersCount }
+        |> Seq.map (isNoticed state delay)
+        |> Seq.forall not
+
+    let rec findDelay delay =
+        if isWalkSafe initialState delay
+        then delay
+        else findDelay (delay + 1)
+
+    findDelay 0
